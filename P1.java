3d03ddd48200d9b79ece46d0964bd10612302596
Amber Westlund <amberwestlund@Ambers-MacBook-Pro.local>
2019-09-08 15:50:53 -0500

    mostly done

--- P1.java
+++ P1.java
@@ -58,5 +58,136 @@ public class P1 {
         if (toString != "String") {
             System.out.println("Sym getType failed type was supposed to be String but accutally was " + toString);
         }
+
+
+        //This is the constructor; it should initialize the SymTable's List field to contain a single, empty HashMap.
+        //SymTable(){}
+        try{
+            SymTable testSymTable = new SymTable();
+        } catch (Exception e){
+            System.out.println("SymTable contructor failed with the following exception:" + e);
+        }
+
+        // test to see if the hashmap is empty
+        try{
+            SymTable testSymTable = new SymTable();
+            testSymTable.removeScope();
+            System.out.println("SymTable contructor failed to make an empty hashmap");
+        } catch (EmptySymTableException e){}
+
+        //If this SymTable's list is empty, throw an EmptySymTableException.
+        try{
+            SymTable testSymTable = new SymTable();
+            testSymTable.addDecl("name", testSym);
+            System.out.println("SymTable's addDecl does not throw an EmptySymTableException");
+        } catch (EmptySymTableException e){}
+        //If either name or sym (or both) is null, throw a NullPointerException.
+        try{
+            SymTable testSymTable = new SymTable();
+            testSymTable.addDecl("", testSym);
+            System.out.println("SymTable's addDecl does not throw a NullPointerException");
+        } catch (EmptySymTableException | NullPointerException e){}
+        try{
+            SymTable testSymTable = new SymTable();
+            Sym nullSym = new Sym("String");
+            testSymTable.addDecl("name", nullSym);
+            System.out.println("SymTable's addDecl does not throw a NullPointerException");
+        } catch (EmptySymTableException | NullPointerException e){}
+        try{
+            SymTable testSymTable = new SymTable();
+            Sym nullSym = new Sym("String");
+            testSymTable.addDecl("", nullSym);
+            System.out.println("SymTable's addDecl does not throw a NullPointerException");
+        } catch (EmptySymTableException | NullPointerException e){}
+        //If the first HashMap in the list already contains the given name as a key, throw a DuplicateSymException. 
+        try{
+            SymTable testSymTable = new SymTable();
+            testSymTable.addDecl("name", testSym);
+            testSymTable.addDecl("name", testSym);
+            System.out.println("SymTable's addDecl does not throw a DuplicateSymException");
+        } catch (EmptySymTableException | DuplicateSymException e){}
+        //Otherwise, add the given name and sym to the first HashMap in the list.
+        try{
+            SymTable testSymTable = new SymTable();
+            testSymTable.addDecl("name1", testSym);
+        } catch (Exception e){
+            System.out.println("SymTable addDecl failed with the following exception:" + e);
+        }
+
+        //Add a new, empty HashMap to the front of the list.
+        try{
+            SymTable testSymTable = new SymTable();
+            testSymTable.addScope();
+        } catch (Exception e){
+            System.out.println("SymTable addScope() failed with the following exception:" + e);
+        }
+
+        //If this SymTable's list is empty, throw an EmptySymTableException. 
+        //Otherwise, if the first HashMap in the list contains name as a key, return the associated Sym; otherwise, return null.
+        try{
+            SymTable testSymTable1 = new SymTable();
+            Sym returnSym = testSymTable1.lookupLocal("name2");
+            System.out.println("SymTable's lookupLocal does not throw an EmptySymTableException");
+        } catch (EmptySymTableException e){}
+
+        try{
+            SymTable testSymTable = new SymTable();
+            testSymTable.addDecl("name2", testSym);
+            Sym returnSym = testSymTable.lookupLocal("name2");
+            if (returnSym == null){
+                System.out.println("SymTable lookupLocal failed");
+            }
+            if (returnSym != testSym){
+                System.out.println("SymTable lookupLocal failed, it returned the wrong Sym");
+            }
+        } catch (Exception e){
+            System.out.println("SymTable lookupLocal failed with the following exception:" + e);
+        }
+
+        //If this SymTable's list is empty, throw an EmptySymTableException. 
+        //If any HashMap in the list contains name as a key, return the first associated Sym (i.e., the one from the HashMap that is closest to the front of the list); 
+        //otherwise, return null.
+        try{
+            SymTable testSymTable2 = new SymTable();
+            Sym returnSym = testSymTable2.lookupGlobal("name2");
+            System.out.println("SymTable's lookupGlobal does not throw an EmptySymTableException");
+        } catch (EmptySymTableException e){}
+
+        try{
+            SymTable testSymTable = new SymTable();
+            testSymTable.addDecl("name3", testSym);
+            Sym returnSym = testSymTable.lookupGlobal("name3");
+            if (returnSym == null){
+                System.out.println("SymTable lookupGlobal failed");
+            }
+            if (returnSym != testSym){
+                System.out.println("SymTable lookupGlobal failed, it returned the wrong Sym");
+            }
+            Sym returnSym2 = testSymTable.lookupGlobal("name4");
+            if (returnSym2 != null){
+                System.out.println("SymTable lookupGlobal failed, it didnt return null");
+            }
+        } catch (EmptySymTableException e){
+            System.out.println("SymTable lookupGlobalfailed with the following exception:" + e);
+        }
+
+        //If this SymTable's list is empty, throw an EmptySymTableException; 
+        //otherwise, remove the HashMap from the front of the list. 
+        //To clarify, throw an exception only if before attempting to remove, the list is empty (i.e. there are no HashMaps to remove).
+        //void removeScope() throws EmptySymTableException{}
+
+        try{
+            SymTable testSymTable3 = new SymTable();
+            testSymTable3.removeScope();
+            System.out.println("SymTable's removeScope() does not throw an EmptySymTableException");
+        } catch (EmptySymTableException e){}
+
+        try{
+            SymTable testSymTable = new SymTable();
+            testSymTable.addDecl("name3", testSym);
+            testSymTable.removeScope();
+            testSymTable.lookupLocal("name3");
+            System.out.println("SymTable's removeScope() does not tclear the list");
+        } catch (EmptySymTableException e){}
     }
 }
--- SymTable.java
+++ SymTable.java
@@ -1,38 +1,83 @@
 import java.util.*;
 
 public class SymTable{
+    private  List<HashMap<String,Sym>> table;
     //This is the constructor; it should initialize the SymTable's List field to contain a single, empty HashMap.
-    SymTable(){}
+    SymTable(){
+        addScope();
+    }
 
     //If this SymTable's list is empty, throw an EmptySymTableException. 
     //If either name or sym (or both) is null, throw a NullPointerException. 
     //If the first HashMap in the list already contains the given name as a key, throw a DuplicateSymException. 
     //Otherwise, add the given name and sym to the first HashMap in the list.
-    void addDecl(String name, Sym sym) throws DuplicateSymException, EmptySymTableException {}
+    void addDecl(String name, Sym sym) throws DuplicateSymException, EmptySymTableException {
+        if(table.isEmpty()){
+            throw EmptySymTableException;
+        }
+        if(name == "" | name == null){
+            throw NullPointerException;
+        }
+        if(lookupLocal(name) != null){
+            throw DuplicateSymException;
+        }
+        addScope();
+        table.get(0).put(name, sym);
+    }
 
     //Add a new, empty HashMap to the front of the list.
-    void addScope(){}
+    void addScope(){
+        HashMap newHashMap = new HashMap<String,Sym>();
+        table.add(0, newHashMap);
+    }
 
     //If this SymTable's list is empty, throw an EmptySymTableException. 
     //Otherwise, if the first HashMap in the list contains name as a key, return the associated Sym; otherwise, return null.
-    Sym lookupLocal(String name){
+    Sym lookupLocal(String name) throws EmptySymTableException{
+        if (table.isEmpty()){
+            throw EmptySymTableException;
+        }
+        HashMap firstEntry = table.get(0);
+        if(firstEntry.containsKey(name)){
+            Sym value = table.get(0).get(name);
+            return value;
+        }
         return null;
     }
 
     //If this SymTable's list is empty, throw an EmptySymTableException. 
     //If any HashMap in the list contains name as a key, return the first associated Sym (i.e., the one from the HashMap that is closest to the front of the list); 
     //otherwise, return null.
-    Sym lookupGlobal(String name){
+    Sym lookupGlobal(String name) throws EmptySymTableException{
+        if (table.isEmpty()){
+            throw EmptySymTableException;
+        }
+        for (HashMap<String,Sym> entry : table){
+            if (entry.containsKey(name)){
+                return entry.get(name);
+            }
+        }
         return null;
     }
 
     //If this SymTable's list is empty, throw an EmptySymTableException; 
     //otherwise, remove the HashMap from the front of the list. 
     //To clarify, throw an exception only if before attempting to remove, the list is empty (i.e. there are no HashMaps to remove).
-    void removeScope() throws EmptySymTableException{}
+    void removeScope() throws EmptySymTableException{
+        if (table.isEmpty()){
+            throw EmptySymTableException;
+        }
+        table.clear();
+    }
 
     //This method is for debugging. First, print “\nSym Table\n”. 
     //Then, for each HashMap M in the list, print M.toString() followed by a newline. 
     //Finally, print one more newline. All output should go to System.out.
-    void print(){}
+    void print(){
+        System.out.println("\nSym Table\n");
+        for (HashMap<String,Sym> entry : table){
+            System.out.println(entry.toString());
+        }
+        System.out.println("");
+    }
 }
